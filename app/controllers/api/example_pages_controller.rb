280d82e4da588c745fe51cf5a6cd6f7498d11582
Micah Sterling <msterling89@gmail.com>
2021-02-22 15:51:56 -0600

    show random fourtune

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,5 +1,8 @@
 class Api::ExamplePagesController < ApplicationController
+  
   def fortune_teller
+    fortune = ["You will do great things", "Now is the time to try something new", "A lifetime friend shall soon be made"]
+    @selected_fortune = fortune.sample
     render 'fortune_teller.json.jb'
   end
 end
